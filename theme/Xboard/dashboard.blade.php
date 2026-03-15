8e38e61332d2ed9c5f4ccbfbb58d965468908233
farmer6 <farmer6@gmail.com>
2026-03-15 23:58:15 +0800

    feat: switch dashboard welcome assets to v8

--- theme/Xboard/dashboard.blade.php
+++ theme/Xboard/dashboard.blade.php
@@ -18,8 +18,8 @@
   };
 </script>
 <script defer src="/vvcloud-custom/vvcloud-contact-float.js"></script>
-<link rel="stylesheet" href="/vvcloud-custom/vvcloud-dashboard-welcome.v4.css">
-<script defer src="/vvcloud-custom/vvcloud-dashboard-welcome.v4.js"></script>
+<link rel="stylesheet" href="/vvcloud-custom/vvcloud-dashboard-welcome.v8.css">
+<script defer src="/vvcloud-custom/vvcloud-dashboard-welcome.v8.js"></script>
 
 <!-- vvcloud:ses-ux:begin -->
 <link rel="stylesheet" href="/vvcloud-custom/vvcloud-email-verify-confirm.v2.css">
